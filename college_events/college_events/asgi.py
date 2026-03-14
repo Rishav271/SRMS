6c66ca7521e74d092c64fd76fe1dad5c26517d81
Rishav271 <rishavrajgupta6@gmail.com>
2026-03-14 18:33:19 +0530

    fixed settings module path

--- college_events/college_events/asgi.py
+++ college_events/college_events/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'college_events.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'college_events.college_events.settings')
 
 application = get_asgi_application()
--- college_events/college_events/wsgi.py
+++ college_events/college_events/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'college_events.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'college_events.college_events.settings')
 
 application = get_wsgi_application()
